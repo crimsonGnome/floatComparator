43aeb066b5b2bebafd698af23c45434058d3acb0
Joseph Eggers <joseph.eggers@csu.fullerton.edu>
2022-08-21 22:08:59 -0700

    initial commit

---
+++ floatComparator.cpp
@@ -0,0 +1,37 @@
+// Joseph Eggers
+// CWID 885939488
+// email: joseph.eggers@csu.fullteron.edu
+
+#include <iostream>
+
+using std::cout, std::cin, std::endl;
+
+int main()
+{
+    float number1;
+    float number2;
+    cout << "Welcome to Floating Points Numbers programmed by Joseph Eggers.\n
+             Ms Silverman has been working for the Longstreet Software Company for 
+             the last two years.\n Please enter two float numbers separated by white
+             space. Press enter after the second input.\n\n Please enter two float 
+             numbers separated by white space. Press enter after the second input.\n\n"
+    
+    // Get float inputs
+    cin >> number1 >> endl;
+    cin >> number2 >> endl;
+
+    cout << "\nThese numbers were entered:\n" << number1 << endl << number2 << endl
+         << endl;
+
+    //x86 program
+    float answer = ;
+    
+    cout << "The larger number is " << answer << endl << endl;
+
+    cout << "This assembly module will now return execution to the driver module.\n";
+         << "The smaller number will be returned to the driver.\n\n"
+         << "The driver module received this float number" << lower << "and will keep it.\n"
+         << "The driver module will return integer 0 to the operating system.\n"
+         << "Have a nice day. Good-bye.";
+
+}
